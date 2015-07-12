b1870bf7f438fc5dcca806abfb2a07a84254cf5b
Alex Arendsen <alex.arendsen.7@gmail.com>
2015-07-12 14:22:33 -0400

    Restyled header

--- edit.php
+++ edit.php
@@ -2,9 +2,13 @@
 
 	require_once 'init.php';
 
-	if(isset($_GET['i'])) {
-		$arg['fileId'] = $_GET['i'];
+	if($arg['loggedIn']) {
+		if(isset($_GET['i'])) {
+			$arg['fileId'] = $_GET['i'];
+		}
+		echo $twig->render("edit.html",$arg);
+	} else {
+		header('Location: index.php');
 	}
-	echo $twig->render("edit.html",$arg);
 
 ?>
