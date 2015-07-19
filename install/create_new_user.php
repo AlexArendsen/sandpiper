5238943c76a9fdd6ca2de5b445e54a5f82c57707
Alex Arendsen <alex.arendsen.7@gmail.com>
2015-07-18 22:21:03 -0400

    User creation script fix

--- install/create_new_user.php
+++ install/create_new_user.php
@@ -2,7 +2,7 @@
 	if(isset($argv[2])){
 		$pwd = password_hash($argv[2],PASSWORD_BCRYPT);
 		$userId = uniqid();
-		echo "INSERT INTO USERS (PUBLIC_ID, USERNAME, PASSWORD, ISADMIN) VALUES ('$uniqId','".$argv[1]."','$pwd',1)";
+		echo "INSERT INTO USERS (PUBLIC_ID, USERNAME, PASSWORD, ISADMIN) VALUES ('$userId','".$argv[1]."','$pwd',1)";
 	} else {
 		echo "Usage: php ".$argv[0]." <username> <password>";
 	}
